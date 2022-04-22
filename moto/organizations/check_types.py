b9cc2e135f0927b57697a903f16029963ae5b098
Iain Samuel McLean Elder <iain@isme.es>
2022-04-22 11:06:42 +0200

    Add experiments with DeepDiff

--- moto/organizations/check_types.py
+++ moto/organizations/check_types.py
@@ -1,15 +1,31 @@
 from pprint import pprint
 import inspect
-
+from deepdiff import DeepDiff
 from mypy_boto3_organizations import OrganizationsClient
-from .models import OrganizationsBackend
+from moto.organizations.models import OrganizationsBackend
 
 boto3_function = OrganizationsClient.create_organization
 moto_function = OrganizationsBackend.create_organization
 
-pprint(inspect.getfullargspec(boto3_function))
-pprint(inspect.getfullargspec(moto_function))
+boto3_spec = inspect.getfullargspec(boto3_function)
+moto_spec = inspect.getfullargspec(moto_function)
+
+# TODO: add pytest tests to check that the backend methods have the same signature as the original client
+pprint(DeepDiff(boto3_spec, moto_spec))
 
 # TODO: add pytest tests to check that the patched type and the original type are the same except for the ResponseMetadata key
-# TODO: add pytest tests to check that the backend methods have the same signature as the original client
-# Use pytest's metafunc for this
+import mypy_boto3_organizations.type_defs as btd
+import moto.organizations.type_defs as mtd
+
+boto_type = btd.CreateOrganizationResponseTypeDef
+moto_type = mtd.CreateOrganizationResponseTypeDef
+
+DeepDiff(boto_type.__annotations__, moto_type.__annotations__)
+
+
+boto_type = btd.DescribeOrganizationResponseTypeDef
+moto_type = mtd.DescribeOrganizationResponseTypeDef
+
+DeepDiff(boto_type.__annotations__, moto_type.__annotations__)
+
+# TODO: Use pytest's metafunc for this
