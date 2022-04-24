281eb3a03fead4ca10a3c5c30a45f67066b936db
Iain Samuel McLean Elder <iain@isme.es>
2022-04-24 16:59:20 +0200

    Simplify del_from_dict_node

--- moto/organizations/write_type_defs.py
+++ moto/organizations/write_type_defs.py
@@ -30,21 +30,14 @@ def del_from_all_dict_nodes(tree: ast.AST, *, keys: Iterable[str]) -> None:
 
 
 def del_from_dict_node(node: ast.Dict, keys: Iterable[str]) -> None:
-    # This would have been a list comprehension, but mypy requires casts that
-    # made it ugly.
-    items_to_delete = list(iter_indexes_to_delete(node, keys))
-    # Go backwards to avoid deleting the wrong thing or failing with an IndexError.
-    # Deletion reindexes subsequent nodes.
-    for i in reversed(items_to_delete):
-        del node.keys[i]
-        del node.values[i]
+    filtered_dict = {
+        k: v
+        for k, v in zip(node.keys, node.values)
+        if cast(str, cast(ast.Constant, k).value) not in keys
+    }
 
-
-def iter_indexes_to_delete(node: ast.Dict, keys: Iterable[str]) -> Iterable[int]:
-    for i, k in enumerate(node.keys):
-        const = cast(ast.Constant, k)
-        if cast(str, const.value) in keys:
-            yield i
+    node.keys = list(filtered_dict.keys())
+    node.values = list(filtered_dict.values())
 
 
 def replace_relative_import(tree: ast.AST, *, relative: str, absolute: str) -> None:
