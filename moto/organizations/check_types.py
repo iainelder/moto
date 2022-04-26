8624b7cd34db7e5503af5e574d64d5d696fd88b3
Iain Samuel McLean Elder <iain@isme.es>
2022-04-26 02:18:02 +0200

    Add tests for org types

--- moto/organizations/check_types.py
+++ moto/organizations/check_types.py
@@ -1,31 +1,54 @@
-from pprint import pprint
+from dataclasses import dataclass
+from typing import Callable, Optional, TypedDict, cast
+from pytest import Metafunc, fail
 import inspect
-from deepdiff import DeepDiff
 from mypy_boto3_organizations import OrganizationsClient
 from moto.organizations.models import OrganizationsBackend
 
-boto3_function = OrganizationsClient.create_organization
-moto_function = OrganizationsBackend.create_organization
 
-boto3_spec = inspect.getfullargspec(boto3_function)
-moto_spec = inspect.getfullargspec(moto_function)
-
-# TODO: add pytest tests to check that the backend methods have the same signature as the original client
-pprint(DeepDiff(boto3_spec, moto_spec))
-
-# TODO: add pytest tests to check that the patched type and the original type are the same except for the ResponseMetadata key
-import mypy_boto3_organizations.type_defs as btd
-import moto.organizations.type_defs as mtd
-
-boto_type = btd.CreateOrganizationResponseTypeDef
-moto_type = mtd.CreateOrganizationResponseTypeDef
-
-DeepDiff(boto_type.__annotations__, moto_type.__annotations__)
+@dataclass
+class APIPair:
+    model_api: Optional[Callable]
+    boto3_api: Callable
 
 
-boto_type = btd.DescribeOrganizationResponseTypeDef
-moto_type = mtd.DescribeOrganizationResponseTypeDef
+def pytest_generate_tests(metafunc: Metafunc):
 
-DeepDiff(boto_type.__annotations__, moto_type.__annotations__)
+    api_names = [name for name in dir(OrganizationsClient) if not name.startswith("_")]
+    api_pairs = [_pair_model_and_boto3_apis(name) for name in api_names]
+    metafunc.parametrize("pair", api_pairs, ids=api_names)
 
-# TODO: Use pytest's metafunc for this
+
+def _pair_model_and_boto3_apis(api_name: str) -> APIPair:
+    return APIPair(
+        model_api=cast(Callable, getattr(OrganizationsBackend, api_name, None)),
+        boto3_api=cast(Callable, getattr(OrganizationsClient, api_name)),
+    )
+
+
+def test_model_mimics_boto3_api_return_type_name(pair: APIPair):
+    if pair.model_api is None:
+        fail("Model API not implemented.")
+
+    model_return_type = get_return_type(pair.model_api)
+    boto3_return_type = get_return_type(pair.boto3_api)
+    assert model_return_type.__name__ == boto3_return_type.__name__
+
+
+def test_model_returns_all_keys_except_response_metadata_and_next_token(pair: APIPair):
+    if pair.model_api is None:
+        fail("Model API not implemented.")
+
+    model_return_type = get_return_type(pair.model_api)
+    boto3_return_type = get_return_type(pair.boto3_api)
+
+    ignored_keys = ["ResponseMetadata", "NextToken"]
+    boto3_required_keys = {
+        k for k in boto3_return_type.__required_keys__ if k not in ignored_keys
+    }
+
+    assert model_return_type.__required_keys__ == boto3_required_keys
+
+
+def get_return_type(func: Callable) -> TypedDict:
+    return inspect.getfullargspec(func).annotations["return"]
